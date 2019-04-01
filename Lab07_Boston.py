21e8211d77044a9045ff5264e4a8f9818ad914eb
bostonheaford <46756585+bostonheaford@users.noreply.github.com>
2019-04-01 03:36:32 -0500

    Add files via upload

--- Lab07_Boston.py
+++ Lab07_Boston.py
@@ -21,14 +21,36 @@ import matplotlib.pyplot as plt
 
 
 def calc_frequencies_and_modes(matrix, k_over_m):
+    """
+    This function calculates the frequency and eigenvectors of the spring setup
+    
+    Parameters
+    ----------
+    matrix: Array
+        A 2x2 matrix corresponding to the the setup of springs we are simulating
+    k_over_m: float
+        The constant defined by the spring constant over the mass of the each of the masses
+    
+    Returns
+    -------
+    freq0: float
+        the first frequency of the spring setup
+    freq1: float
+        the second frequency of the spring setup
+    eigenvector: Array
+        An array containing the 2 eigenvectors associated with this spring setup
+    """
     eigenvalue,eigenvector=eigh(k_over_m*matrix)
     freq0=np.sqrt(np.abs(eigenvalue[0]))
     freq1=np.sqrt(np.abs(eigenvalue[1]))
     return freq0, freq1, eigenvector
 
-
+N_times=1000
+t_init=0
+t_end=10
 matrix1=np.array([[-2,1],[1,-2]])
-time=np.arange(0,10,.01)
+time=np.linspace(t_init,t_end,num=N_times)
+time=time.reshape(N_times,1)
 
 """
 print(calc_frequencies_and_modes(matrix1,1)[0])
@@ -37,21 +59,58 @@ print(calc_frequencies_and_modes(matrix1,1)[2])
 """
 
 def calc_components_from_initial_conditions(x_init,modes): #modes are eigenvectors
+    """
+    Function to calculate the components of the equation of motion
+    
+    Parameters
+    ----------
+    x_init: array of initial starting points
+        This is the array of the starting points for the masses on the spring.
+        Positive values mean the mass is moved to the right of the equilibrium position.
+    modes: array 
+        an array that contains the eigenvectors for the spring setup.
+        
+    Returns
+    -------
+    a: float
+        one of the components to the equation of motion
+    b: float
+        one of the other components to the equation of motion
+    """
     a=x_init@modes[0]
     b=x_init@modes[1]
     return a, b
 
 
 def position_of_masses(x_init, time, matrix, k_over_m):
+    """
+    Function to calculate the position's of the masses on the springs
+    over time. 
+    
+    Parameters
+    ----------
+    x_init: array of initial starting points
+        This is the array of the starting points for the masses on the spring.
+        Positive values mean the mass is moved to the right of the equilibrium position.
+    time : array of times
+        Times at which the positions have been calculated.
+    matrix: Array
+        A 2x2 matrix corresponding to the the setup of springs we are simulating
+    k_over_m: float
+        The constant defined by the spring constant over the mass of the each of the masses
+        
+    Returns:
+    --------
+    x: Array
+        an array that contains the positions of the two masses over time
+    """
     freq0,freq1,eigenvector=calc_frequencies_and_modes(matrix, k_over_m)
     a,b=calc_components_from_initial_conditions(x_init,eigenvector)
     x=a*np.cos(freq0*time)*eigenvector[0]+b*np.cos(freq1*time)*eigenvector[1]
     return x
     
-position=position_of_masses([1,1], time, matrix1, 1)
 
-
-def plot_motion_of_masses(x, time, title='bad title'):
+def plot_motion_of_masses(x, time, title='Motion of Springs'):
     """
     Function to make a plot of motion of masses as a function of time. The time
     should be on the vertical axis and the position on the horizontal axis.
@@ -75,13 +134,54 @@ def plot_motion_of_masses(x, time, title='bad title'):
     plt.plot(x1, time, label='Mass 1')
     plt.plot(x2, time, label='Mass 2')
     plt.xlim(0, 9)
+    plt.xlabel('Position')
+    plt.ylabel('Time')
     plt.legend()
     plt.title(title)
+    plt.show()
 
-print(calc_components_from_initial_conditions(np.array([.1,0]),np.array([.70710678,.70710678])))
+position0=position_of_masses([1,-1], time, matrix1, 1)
+position1=position_of_masses([1,1], time, matrix1, 1)
+position2=position_of_masses([1,0], time, matrix1, 1)
+position3=position_of_masses([0,1], time, matrix1, 1)
+position4=position_of_masses([-.82,.3], time, matrix1, 1)
 
+plot_motion_of_masses(position0,time)
+plot_motion_of_masses(position1,time)
+plot_motion_of_masses(position2,time)
+"""
+This plot doesn't oscillate as a pure sine wave, its harder to predict
+"""
+plot_motion_of_masses(position3,time)
+"""
+This also doesn't oscillate as a pure sine wave, though its not the same
+as the previous case since the initial conditions aren't equivalent
+"""
+plot_motion_of_masses(position4,time)
 
+"""
+Answer for part 5:
+The  matrix should remain the same, but the coefficient in front of the matrix should change
+The springs should oscillate just like they did before, except with a higher frequency. 
+"""
+position5=position_of_masses([1,1], time, matrix1, 10)
+position6=position_of_masses([1,-1], time, matrix1, 10)
+position7=position_of_masses([1,0], time, matrix1, 10)
+position8=position_of_masses([0,1], time, matrix1, 10)
 
+plot_motion_of_masses(position5,time, 'Heavy Masses') 
+plot_motion_of_masses(position6,time, 'Heavy Masses')
+"""
+The motion when they are set to their eigenvectors is either in phase or antiphase, 
+just like what you would expect
+"""
+plot_motion_of_masses(position7,time, 'Heavy Masses')
+plot_motion_of_masses(position8,time, 'Heavy Masses')
+"""
+These are different because they are not eigenvectors and therefore wont have a motion 
+that ends up in phase or antiphase, its something that oscillates in a way thats not a pure
+sine wave
+"""
 
 
 
